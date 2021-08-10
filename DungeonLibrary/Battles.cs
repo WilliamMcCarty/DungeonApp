37425efd2e9ccc63e211a88462d56401b73b5989
William McCarty <kcchief@yahoo.com>
2021-08-10 13:08:31 -0500

    Added the battles class to hold the battles for the program

---
+++ DungeonLibrary/Battles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Battles
+    {
+        public static string GetBattle()
+        {
+            //Declare & Intialize battle array
+            string[] battle =
+            {
+                "Battle of the Morannon.",
+                "Battle of the Black Gate.",
+                "Battle for Middle-earth.",
+                "Battle Of Helms Deep.",
+                "Battle For Minas Tirith.",
+                "Battle of the Pelennor Fields.",
+                "Battle of the Hornburg.",
+                "Battle of Bywater."
+            };
+
+            //Declare random
+            Random random = new Random();
+            //Declare & Intialize index from a random number up to array length
+            int index = random.Next(battle.Length);
+            //Declare & Intialize battles getting the battle from the random index
+            string battles = "You Have A New Battle...\n\n" + battle[index] + "\n";
+            //Return battles
+            return battles;
+        }//end GetBattle
+    }//end Class Battles
+}//end NameSpace
