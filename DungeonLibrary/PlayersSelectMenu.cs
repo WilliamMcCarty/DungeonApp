666444621313de9f192033bc8d1629156e3a8c36
William McCarty <kcchief@yahoo.com>
2021-08-10 14:15:36 -0500

    Added the new playersselectmenu with new updated players

---
+++ DungeonLibrary/PlayersSelectMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class PlayerMenu
+    {
+        public static void PlayerSelectMenu()
+        {
+            Console.WriteLine("Choose Your Character:\n" + "1) Human\n2) Elf\n3) Dwarf\n4) Goblin\n5) Wizard\n6) Hobbit");
+        }//end PlayerSelectMenu()
+    }//end Class PlayerMenu
+}//end NameSpace
